e3a55c8460cedc56bd421d3e2bc0d423a6ff728e
wan_yu_chen <wan_yu_chen@126.com>
2022-03-04 23:09:20 +0800

    add image

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+// 字体图标
+import '../public/assets/fonts/iconfont.css'
+
 const app = createApp(App)
     .use(IonicVue)
     .use(router)
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,96 +1,124 @@
 <template>
-    <ion-page>
-        <ion-header :translucent="true">
-            <ion-toolbar>
-                <ion-title>{{ $t('system.title') }}:{{ language }}</ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content :fullscreen="true">
-            <div id="container">
-                <div>
-                    <ion-button color="secondary" @click="getSomethings">TEST AXIOS</ion-button>
-                </div>
-                <div>
-                    <ion-button color="secondary" @click="changeLanguage">CHANGE LANG</ion-button>
-                </div>
-                <strong>Ready to create an app?</strong>
-                <p>Start with Ionic <a target="_blank" rel="noopener noreferrer"
-                                       href="https://ionicframework.com/docs/components">UI Components</a></p>
-            </div>
-        </ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>{{ $t('system.title') }}:{{ language }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <div id="container">
+        <div>
+          <ion-button color="secondary" @click="getSomethings"
+            >TEST AXIOS</ion-button
+          >
+        </div>
+        <div>
+          <ion-button color="secondary" @click="changeLanguage"
+            >CHANGE LANG</ion-button
+          >
+        </div>
+        <strong>Ready to create an app?</strong>
+        <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
+        <div
+          class="icon iconfont icon-lingdang"
+          style="width: 100px; height: 100px"
+        ></div>
+        <img :src="imgUrl1" />
+        <img :src="imgUrl2" />
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton} from '@ionic/vue';
-import {computed, defineComponent} from 'vue';
-import {useStore} from "vuex";
-import {useI18n} from "vue-i18n";
-import {testGet} from "@/api/test";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+} from '@ionic/vue'
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
+import { useI18n } from 'vue-i18n'
+import { testGet } from '@/api/test'
 
 export default defineComponent({
-    components: {
-        IonContent,
-        IonHeader,
-        IonPage,
-        IonTitle,
-        IonToolbar,
-        IonButton
-    },
-    setup() {
-        const store = useStore();
-        const { locale } = useI18n();
-        const language = computed(() => store.getters["languageModule/getLanguage"]);
-        const getSomethings = () => {
-            testGet().then(res => {
-                console.log(res)
-            })
-        }
-        const changeLanguage = () => {
-            if (locale.value === 'en') {
-                locale.value = 'zh'
-                store.dispatch('languageModule/updateLanguage', locale.value)
-            } else {
-                locale.value = 'en'
-                store.dispatch('languageModule/updateLanguage', locale.value)
-            }
-        }
-        return {
-            language,
-            changeLanguage,
-            getSomethings
-        }
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonButton,
+  },
+  setup() {
+    const store = useStore()
+    const { locale } = useI18n()
+    const language = computed(() => store.getters['languageModule/getLanguage'])
+    const getSomethings = () => {
+      testGet().then((res) => {
+        console.log(res)
+      })
     }
-});
+    const changeLanguage = () => {
+      if (locale.value === 'en') {
+        locale.value = 'zh'
+        store.dispatch('languageModule/updateLanguage', locale.value)
+      } else {
+        locale.value = 'en'
+        store.dispatch('languageModule/updateLanguage', locale.value)
+      }
+    }
+    // const imgUrl1 = `./images/home.png`
+    return {
+      language,
+      changeLanguage,
+      getSomethings,
+      imgUrl1: `assets/images/home.png`,
+      // imgUrl1:"/home.png",
+      imgUrl2: `assets/images/way.png`,
+    }
+  },
+})
 </script>
 
 <style lang="scss" scoped>
-@import "src/common/layout";
+@import 'src/common/layout';
 #container {
-    text-align: center;
+  text-align: center;
 
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 50%;
-    transform: translateY(-50%);
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
 }
 
 #container strong {
-    font-size: 20px;
-    line-height: 26px;
+  font-size: 20px;
+  line-height: 26px;
 }
 
 #container p {
-    font-size: 16px;
-    line-height: 22px;
+  font-size: 16px;
+  line-height: 22px;
 
-    color: #8c8c8c;
+  color: #8c8c8c;
 
-    margin: 0;
+  margin: 0;
 }
 
 #container a {
-    text-decoration: none;
+  text-decoration: none;
 }
 </style>
